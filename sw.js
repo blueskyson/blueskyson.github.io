8c78d4bdfe26eaefeb808b88f4476d57c17bd9df
Jack Lin <jacklin@cybersoft4u.com>
2023-06-05 00:01:11 +0800

    Update images

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@ const PRECACHE_LIST = [
   "./img/icon_wechat.png",
   "./img/avatar-hux.jpg",
   "./img/home-bg.jpg",
-  "./img/404-bg.jpg",
+  "./img/head-bg.jpg",
   "./css/hux-blog.min.css",
   "./css/bootstrap.min.css"
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
